d1caad4e4588ef97a0fb4f9b1330bdd43b0f2697
enpit-student <enpit-student@es.local>
2016-10-26 21:13:59 +0900

    Initial spring boot project

---
+++ src/main/java/chou/cloud/datacenter/ChouCloudDatacenterApplication.java
@@ -0,0 +1,12 @@
+package chou.cloud.datacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChouCloudDatacenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChouCloudDatacenterApplication.class, args);
+	}
+}
