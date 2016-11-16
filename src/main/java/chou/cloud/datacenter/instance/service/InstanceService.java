c82fd94175b90173f4f51aabe01e3f8bb9391ade
enpit-student <enpit-student@es.local>
2016-11-16 20:15:52 +0900

    Repush instance service

--- src/main/java/chou/cloud/datacenter/instance/service/InstanceService.java
+++ src/main/java/chou/cloud/datacenter/instance/service/InstanceService.java
@@ -1,5 +1,6 @@
-package chou.cloud.datacenter.chou.cloud.datacenter.instance.service;
+package chou.cloud.datacenter.instance.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import chou.cloud.datacenter.chou.cloud.datacenter.instance.entity.Instance;
-import chou.cloud.datacenter.chou.cloud.datacenter.instance.repository.InstanceRepository;
+import chou.cloud.datacenter.common.Consts;
+import chou.cloud.datacenter.instance.entity.Instance;
+import chou.cloud.datacenter.instance.repository.InstanceRepository;
+import chou.cloud.datacenter.vm.service.VirtualMachineService;
 
 @Service
 @Transactional
@@ -16,6 +19,9 @@ public class InstanceService {
 
 	@Autowired
 	InstanceRepository instanceRepository;
+
+	@Autowired
+	VirtualMachineService virtualMachineService;
 
 	public List<Instance> findAll() {
 		return instanceRepository.findAll(new Sort(Sort.Direction.ASC, "id"));
@@ -32,4 +38,71 @@ public class InstanceService {
 	public Instance find(Long id) {
 		return instanceRepository.findOne(id);
 	}
+
+	public List<Instance> findByStatus(String status) {
+		return instanceRepository.findByStatus(status);
+	}
+
+	public List<Instance> findByName(String name) {
+		return instanceRepository.findByName(name);
+	}
+
+	public Instance upInstance(Long id) {
+		Instance instance = instanceRepository.findOne(id);
+		if (Consts.INSTANCE_STATUS_STARTING.equals(instance.getStatus())
+				|| Consts.INSTANCE_STATUS_ACTIVED.equals(instance.getStatus())) {
+			return instance;
+		}
+
+		instance.setStatus(Consts.INSTANCE_STATUS_STARTING);
+		instance = instanceRepository.save(instance);
+
+		virtualMachineService.start(instance);
+
+		return instance;
+	}
+
+	public Instance downInstance(Long id) {
+		Instance instance = instanceRepository.findOne(id);
+		if (Consts.INSTANCE_STATUS_STOPPING.equals(instance.getStatus())
+				|| Consts.INSTANCE_STATUS_INACTIVED.equals(instance.getStatus())) {
+			return instance;
+		}
+
+		instance.setStatus(Consts.INSTANCE_STATUS_STOPPING);
+		instance = instanceRepository.save(instance);
+
+		virtualMachineService.stop(instance);
+
+		return instance;
+	}
+
+	public List<Instance> asupInstance(Long id, int size) {
+		List<Instance> instances = new ArrayList<Instance>(size);
+		Instance instance = upInstance(id);
+		instances.add(instance);
+
+		for (int i = 0; i < size - 1; i++) {
+			instance.setId(null);
+			instance = instanceRepository.save(instance);
+			instance = upInstance(instance.getId());
+			instances.add(instance);
+		}
+
+		return instances;
+	}
+
+	public List<Instance> asdownInstance(Long id) {
+		Instance instance = instanceRepository.findOne(id);
+		List<Instance> instances = instanceRepository.findByName(instance.getName());
+
+		for (int i = 0; i < instances.size(); i++) {
+			if (!instance.getId().equals(instances.get(i).getId())){
+				instances.set(i, downInstance(instances.get(i).getId()));
+			}
+		}
+
+		return instances;
+	}
+
 }
